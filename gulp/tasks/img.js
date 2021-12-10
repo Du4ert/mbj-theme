5632b1655f97c9ab9087db5b36cb9769da7b440a
Du4ert <du4ertb@gmail.com>
2021-12-10 12:21:22 +0300

    kk

--- gulp/tasks/img.js
+++ gulp/tasks/img.js
@@ -1,7 +1,7 @@
 module.exports = function() {
     $.gulp.task('img', function() {
     return $.gulp.src('src/img/**/*')
-        .pipe($.gp.if($.env === 'production', $.gp.cache($.gp.image())))
+        // .pipe($.gp.if($.env === 'production', $.gp.cache($.gp.image())))
         .pipe($.gulp.dest('img'))
     });
 }
